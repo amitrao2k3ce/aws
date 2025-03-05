54c4f21e96e902d9f64de01c23482a06c2916131
AMIT  KUMAR <amitrao2003ce@hotmail.com>
2025-03-05 09:40:08 +0530

    5th edit from local machine

--- terraform_compute_deployment-dev.tf
+++ terraform_compute_deployment-dev.tf
@@ -1,4 +1,4 @@
-# This code block is to initialize the terraform configuration 4th edit
+# This code block is to initialize the terraform configuration 5th edit on VS Code from local system
 terraform {
     required_providers {
         aws = {
